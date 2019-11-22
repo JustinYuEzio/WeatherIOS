7c95f73a33751855eb012a0ac652ceba736b01fb
Hengyi Yu <hengyiyu@usc.edu>
2019-11-22 14:37:16 -0800

    Initial Commit

---
+++ ForecastApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForecastApp
+//
+//  Created by Hengyi Yu on 11/22/19.
+//  Copyright © 2019 Hengyi Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
